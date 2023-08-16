7a691d1b180592a308ee7b4d79470de9200ea943
cgodecke <cgodecke@student.42wolfsburg.de>
2023-08-16 10:06:34 +0200

    changed is_map_end to is_wall (general wall checker)

--- src/mlx/handle_keypress.c
+++ src/mlx/handle_keypress.c
@@ -6,41 +6,48 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 10:45:15 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/08/16 09:49:45 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/08/16 10:04:23 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3D.h"
 
-static int	is_map_end(t_data *data, double step_factor, int button)
+int map2[10][10] = {
+    {1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,1,0,1,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1},
+};
+
+static int	is_wall(t_data *data, double step_factor, int button)
 {
+	double	x;
+	double	y;
+
 	if (button == 'w')
 	{
-		printf("button: %c\n", button);
-		if (data->player_x + cos(deg_to_rad(data->player_angle)) * step_factor
-			> 1 && data->player_x + cos(deg_to_rad(data->player_angle))
-			* step_factor < data->map_columns - 1
-			&& data->player_y - sin(deg_to_rad(data->player_angle))
-			* step_factor < data->map_rows - 1 && data->player_y
-			- sin(deg_to_rad(data->player_angle)) * step_factor > 1)
-			return (0);
+		x = data->player_x + cos(deg_to_rad(data->player_angle)) * step_factor;
+		y = data->player_y - sin(deg_to_rad(data->player_angle)) * step_factor;
+		return (map2[(int)(floor(y))][(int)(floor(x))]);
 	}
 	if (button == 's')
 	{
-		if (data->player_x - cos(deg_to_rad(data->player_angle)) * step_factor
-			> 1 && data->player_x - cos(deg_to_rad(data->player_angle))
-			* step_factor < data->map_columns - 1 && data->player_y
-			+ sin(deg_to_rad(data->player_angle)) * step_factor < data->map_rows
-			- 1 && data->player_y + sin(deg_to_rad(data->player_angle))
-			* step_factor > 1)
-			return (0);
+		x = data->player_x - cos(deg_to_rad(data->player_angle)) * step_factor;
+		y = data->player_y + sin(deg_to_rad(data->player_angle)) * step_factor;
+		return (map2[(int)(floor(y))][(int)(floor(x))]);
 	}
 	return (1);
 }
 
 static void	press_w(t_data *data, double step_factor)
 {
-	if (is_map_end(data, step_factor, 'w') == 0)
+	if (is_wall(data, step_factor, 'w') == 0)
 	{
 		data->player_x += cos(deg_to_rad(data->player_angle)) * step_factor;
 		data->player_y -= sin(deg_to_rad(data->player_angle)) * step_factor;
@@ -65,7 +72,7 @@ int	handle_keypress(int keysym, t_data *data)
 	}
 	else if (keysym == XK_s)
 	{
-		if (is_map_end(data, step_factor, 's') == 0)
+		if (is_wall(data, step_factor, 's') == 0)
 		{
 			data->player_x -= cos(deg_to_rad(data->player_angle)) * step_factor;
 			data->player_y += sin(deg_to_rad(data->player_angle)) * step_factor;
